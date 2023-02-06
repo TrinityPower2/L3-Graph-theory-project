fe54acb737972163a2e99995145ea3a4b2ac6897
Leowenex <antoinecraipeau@outlook.fr>
2023-02-06 15:32:46 +0100

    Initial Commit

---
+++ vertice.py
@@ -0,0 +1,6 @@
+class Vertice:
+
+    def __init__(self, name: str, duration: int, predecessors):
+        self.name = name
+        self.duration = duration
+        self.predecessors = predecessors
