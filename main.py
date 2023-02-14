f72a493b1d05e997dcd390ae4f742964e35a8cdc
Leowenex <antoinecraipeau@outlook.fr>
2023-02-14 10:09:22 +0100

    THE SELECTOR

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import os
-
 import graph as gr
+
+import tkinter as tk
+from tkinter import filedialog
 
 
 def list_graphs():
     print("\nAvailable files:\n")
     for file in os.listdir("graphs/"):
-        print("- "+file.split(".")[0])
+        print("- " + file.split(".")[0])
+
+
+def file_opener(root_window):
+    path = filedialog.askopenfilename(initialdir="./graphs", title="Select a File")
+    return path
 
 
 if __name__ == '__main__':
+
+    # Creates the tkinter window (necessary to summon the file selector)
+    root = tk.Tk()
+    # Hides the tkinter window
+    root.wm_attributes('-alpha', 0.0)
+    # Disables the tkinter windows' button
+    root.wm_attributes('-disabled', True)
+
     running = 1
     while running:
         print("\nWhat action would you like to perform ?")
@@ -22,10 +37,9 @@ if __name__ == '__main__':
                     running = 0
                     break
                 case "1":
-                    list_graphs()
-                    print("\nPlease enter the name of your file without the extension:")
                     try:
-                        active_graph = gr.Graph(open("graphs/" + input() + ".txt", "r"))
+                        print("yo")
+                        active_graph = gr.Graph(open(file_opener(root), "r"))
                     except FileNotFoundError:
                         print("File not found !")
                     break
