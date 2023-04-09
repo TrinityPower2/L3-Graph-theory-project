46a8d0c62d43089dc0fa439f831693e7116b1421
TrinityPower2 <capella.jb2@gmail.com>
2023-04-09 17:09:54 +0200

    Add some comments for clarity

--- negative_edge_detection.py
+++ negative_edge_detection.py
@@ -1,6 +1,6 @@
 
-def has_negative_edge(gr):
-    for vex in gr.vertices:
-        if vex.duration < 0:
+def has_negative_edge(gr):                  # Check if the graph contains negative edges
+    for vex in gr.vertices:         # Browse through all the vertices of the graph
+        if vex.duration < 0:        # Check that the duration is not negative
             return True
-    return False
+    return False                    # Return false if no negative duration found
